7f9c1fe9c4c899792a4d3b0f02766df25953f59a
James No <james.d.no@outlook.com>
2026-08-01 11:08:00 -0700

    Add: validate-parentheses - submission-14

---
+++ Data Structures & Algorithms/validate-parentheses/submission-14.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {"]": "[", "}": "{", ")": "("}
+        stack = []
+        for c in s:
+            if c in pairs:
+                if not stack or stack[-1] != pairs[c]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(c)
+        return not stack
+            
