2104148ae9724dff579e99bef15eae9d4575249b
James No <james.d.no@outlook.com>
2026-08-03 13:24:32 -0700

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,5 @@
+a, b, c = 2, 8, 5
+print((a * b) // c)     # 16//5 = 3.2, round down to 3
+print((a * b) % c)      # 16/5 = 3 with the remainder of 1
+print(a ** b)           # 2 ^ 8 = 256
+print(b ** c)           # 8^5 = 32,768 
